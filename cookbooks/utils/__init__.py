b040cd0c48f3224c1d01cc495b809e7f39f669d0
Lily Zhang <alchemxz@gmail.com>
2025-07-02 01:04:51 +0000

    simplify the notebook

---
+++ cookbooks/utils/__init__.py
@@ -0,0 +1,9 @@
+from .inference import (
+    inference,
+    inference_with_api,
+)
+
+from .video_preprocessing import (
+    get_video_frames,
+    create_image_grid,
+)
---
+++ cookbooks/utils/video_preprocessing.py
@@ -0,0 +1,68 @@
+import os
+import math
+import hashlib
+import requests
+
+from IPython.display import Markdown, display
+import numpy as np
+from PIL import Image
+import decord
+from decord import VideoReader, cpu
+
+
+def download_video(url, dest_path):
+    response = requests.get(url, stream=True)
+    with open(dest_path, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=8096):
+            f.write(chunk)
+    print(f"Video downloaded to {dest_path}")
+
+
+def get_video_frames(video_path, num_frames=128, cache_dir='.cache'):
+    os.makedirs(cache_dir, exist_ok=True)
+
+    video_hash = hashlib.md5(video_path.encode('utf-8')).hexdigest()
+    if video_path.startswith('http://') or video_path.startswith('https://'):
+        video_file_path = os.path.join(cache_dir, f'{video_hash}.mp4')
+        if not os.path.exists(video_file_path):
+            download_video(video_path, video_file_path)
+    else:
+        video_file_path = video_path
+
+    frames_cache_file = os.path.join(cache_dir, f'{video_hash}_{num_frames}_frames.npy')
+    timestamps_cache_file = os.path.join(cache_dir, f'{video_hash}_{num_frames}_timestamps.npy')
+
+    if os.path.exists(frames_cache_file) and os.path.exists(timestamps_cache_file):
+        frames = np.load(frames_cache_file)
+        timestamps = np.load(timestamps_cache_file)
+        return video_file_path, frames, timestamps
+
+    vr = VideoReader(video_file_path, ctx=cpu(0))
+    total_frames = len(vr)
+
+    indices = np.linspace(0, total_frames - 1, num=num_frames, dtype=int)
+    frames = vr.get_batch(indices).asnumpy()
+    timestamps = np.array([vr.get_frame_timestamp(idx) for idx in indices])
+
+    np.save(frames_cache_file, frames)
+    np.save(timestamps_cache_file, timestamps)
+    
+    return video_file_path, frames, timestamps
+
+
+def create_image_grid(images, num_columns=8):
+    pil_images = [Image.fromarray(image) for image in images]
+    num_rows = math.ceil(len(images) / num_columns)
+
+    img_width, img_height = pil_images[0].size
+    grid_width = num_columns * img_width
+    grid_height = num_rows * img_height
+    grid_image = Image.new('RGB', (grid_width, grid_height))
+
+    for idx, image in enumerate(pil_images):
+        row_idx = idx // num_columns
+        col_idx = idx % num_columns
+        position = (col_idx * img_width, row_idx * img_height)
+        grid_image.paste(image, position)
+
+    return grid_image
